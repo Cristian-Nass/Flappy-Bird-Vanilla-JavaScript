84b3cc88b9ae379781f92df2e41985a23346bf12
Cristian Nasirabadi <cristian.tirdad@gmail.com>
2020-10-08 21:36:52 +0200

    animation worked score and best value too

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ const ctx = cvs.getContext("2d");
 
 ///////////////////////////// GAME VARS AND CONSTS
 let frames = 0;
-
+const DEGREE = Math.PI / 180;6
 
 ////////////////////////////// LOAD SPRITE IMAGE
 const sprite = new Image();
@@ -59,10 +59,17 @@ const fg = {
     h: 112,
     x: 0,
     y: cvs.height - 112,
+    dx: 2,
 
     draw : function() {
         ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
         ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w , this.y, this.w, this.h);
+    },
+
+    update: function() {
+        if (state.current == state.game) {
+            this.x = (this.x - this.dx) % (this.w/2);
+        }
     }
 }
 
@@ -78,15 +85,24 @@ const bird = {
     y: 150,
     w: 34,
     h: 26,
+    radius: 12,
     frame: 0,
 
     gravity: 0.25,
     jump: 4.6,
     speed: 0,
+    rotation: 0,
 
     draw : function() {
         let bird = this.animation[this.frame];
-        ctx.drawImage(sprite, bird.sX, bird.sY, this.w, this.h, this.x - this.w/2, this.y - this.h/2 , this.w, this.h);
+
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.rotation);
+
+        ctx.drawImage(sprite, bird.sX, bird.sY, this.w, this.h, - this.w/2, - this.h/2 , this.w, this.h);
+
+        ctx.restore();
     },
 
     flap: function(){
@@ -103,6 +119,7 @@ const bird = {
 
         if (state.current == state.getReady) {
             this.y = 150;
+            this.rotation = 0 * DEGREE;
         } else {
             this.speed += this.gravity;
             this.y += this.speed;
@@ -112,6 +129,13 @@ const bird = {
                 if (state.current == state.game) {
                     state.current = state.over;
                 }
+            }
+            // IF THE SPEED IS GREATER THAN JUMP  MEANS THE BIRD IS FALLING DOWN
+            if (this.speed >= this.jump) {
+                this.rotation = 90 * DEGREE;
+                this.frame = 1;
+            } else {
+                this.rotation = -25 * DEGREE;
             }
         }
     }
@@ -149,22 +173,129 @@ const gameOver = {
         }
     }
 }
+
+////////////////////////////// PIPES
+const pipes = {
+    position: [],
+    top: {  
+        sX: 553,
+        sY: 0
+    },
+
+    bottom: {
+        sX: 502,
+        sY: 0
+    },
+    w: 53,
+    h: 400,
+    gap: 85,
+    maxYPos: -150,
+    dx: 2,
+
+    draw: function() {
+        for(let i = 0; i < this.position.length; i++) {
+            let p= this.position[i];
+            let topYPos = p.y;
+            let bottomYPos = p.y + this.h + this.gap;
+
+            // TOP PIPE
+            ctx.drawImage(sprite, this.top.sX, this.top.sY, this.w, this.h, p.x, topYPos, this.w, this.h);
+            // BOTTOM PIPE
+            ctx.drawImage(sprite, this.bottom.sX, this.bottom.sY, this.w, this.h, p.x, bottomYPos, this.w, this.h);
+        }
+    },
+
+    update: function() {
+        if (state.current !== state.game) return;
+        if (frames%100 == 0) {
+            this.position.push({
+                x: cvs.width,
+                y: this.maxYPos * (Math.random() + 1)
+            });
+        }
+        for (let i = 0; i < this.position.length; i++) {
+            let p = this.position[i];
+           
+
+            let bottomPipeYPos = p.y + this.h + this.gap;
+            // TOP PIPE
+            if (bird.x + bird.radius > p.x && bird.x - bird.radius < p.x + this.w && 
+                bird.y + bird.radius > p.y && bird.y - bird.radius < p.y + this.h) {
+                    state.current = state.over;
+                }
+
+            // BOTTOM PIPE
+            if (bird.x + bird.radius > p.x && bird.x - bird.radius < p.x + this.w && 
+                bird.y + bird.radius > bottomPipeYPos && bird.y - bird.radius < bottomPipeYPos + this.h) {
+                    state.current = state.over;
+                }
+            
+            // MOVE PIPE TO LEFT
+            p.x -= this.dx;
+                
+            // IF PIPES GO BEYOND CANVES, WE DELETE THEM FOR THE ARRAY
+            if (p.x + this.w <= 0) {
+                this.position.shift();
+                score.value += 1;
+                score.best = Math.max(score.value, score.best);
+                localStorage.setItem('best', score.best);
+            }
+        }
+    }
+}
+
+//////////////////////////////////////////// SCORE
+const score = {
+    best : parseInt(localStorage.getItem("best")) || 0,
+    value : 0,
+    
+    draw : function(){
+        ctx.fillStyle = "#FFF";
+        ctx.strokeStyle = "#000";
+        
+        if(state.current == state.game){
+            ctx.lineWidth = 2;
+            ctx.font = "35px Teko";
+            ctx.fillText(this.value, cvs.width/2, 50);
+            ctx.strokeText(this.value, cvs.width/2, 50);
+            
+        }else if(state.current == state.over){
+            // SCORE VALUE
+            ctx.font = "25px Teko";
+            ctx.fillText(this.value, 225, 186);
+            ctx.strokeText(this.value, 225, 186);
+            // BEST SCORE
+            ctx.fillText(this.best, 225, 228);
+            ctx.strokeText(this.best, 225, 228);
+        }
+    },
+    
+    reset : function(){
+        this.value = 0;
+    }
+}
+
+
 ////////////////////////////// DRAW
 function draw() {
     ctx.fillStyle = "#94d2ff"
     ctx.fillRect(0, 0, cvs.clientWidth, cvs.height)
 
     bg.draw();
+    pipes.draw();
     fg.draw();
     bird.draw();
     getReady.draw();
     gameOver.draw();
+    score.draw();
 }
 
 
 ///////////////////////////// UPDATE
 function update() {
       bird.update();
+      fg.update();
+      pipes.update();
 }
 
 
